7969d44020497f171e5b23708b27c6018110ac59
yuseonho <youseonho@naver.com>
2021-10-19 10:47:45 +0900

    잔고갱신 및 매도전략 속도 개선

--- trader/trader.py
+++ trader/trader.py
@@ -195,6 +195,7 @@ class Trader:
                             continue
                     elif len(data) == 6:
                         self.UpdateJango(data[0], data[1], data[2], data[3], data[4], data[5])
+                        continue
                 elif type(data) == str:
                     self.RunWork(data)
 
